a20d2b50fac8f274ca182f1aebd7a84f20b7fd81
Carlos Suárez <mgwump@gmail.com>
2016-05-10 14:53:11 -0700

    let's do this!

--- src/nodo.java
+++ src/nodo.java
@@ -2,4 +2,12 @@ public class nodo {
     String lexema;
     int token;
     int renglon;
+    nodo sig = null;
+    
+    nodo(String palabra, int token, int renglon){
+    this.lexema = lexema;
+    this.token = token;
+    this.renglon = renglon;
+    
+    }
 }
