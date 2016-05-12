5c538d273a2f8af8c738844bcd7504bffbbc463f
Carlos Suárez <mgwump@gmail.com>
2016-05-12 14:35:33 -0700

    it's done!

--- src/compita.java
+++ src/compita.java
@@ -5,9 +5,10 @@ public class compita{
          public static void main(String[] args)throws IOException{
                lexico a = new lexico();
                a.lexic();
-               lexico.p=lexico.cabeza;
-               while (lexico.p!=null) {
-                 System.out.println(lexico.p.lexema+" "+lexico.p.token+" "+lexico.p.renglon);
+               a.p = a.cabeza;
+               while (a.p != null) {
+                 System.out.println(a.p.lexema+" "+a.p.token+" "+a.p.renglon);
+                 a.p = a.p.sig;
              }
                
          }
