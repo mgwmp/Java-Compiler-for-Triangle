18224cfcedc235314c2f09e928d353568e214977
Carlos Suárez <mgwump@gmail.com>
2016-05-12 14:37:40 -0700

    it's done!

--- src/compita.java
+++ src/compita.java
@@ -3,12 +3,12 @@ import java.io.*;
 
 public class compita{
          public static void main(String[] args)throws IOException{
-               lexico a = new lexico();
-               a.lexic();
-               a.p = a.cabeza;
-               while (a.p != null) {
-                 System.out.println(a.p.lexema+" "+a.p.token+" "+a.p.renglon);
-                 a.p = a.p.sig;
+               lexico lexicomain = new lexico();
+               lexicomain.lexic();
+               lexicomain.p = lexicomain.cabeza;
+               while (lexicomain.p != null) {
+                 System.out.println(lexicomain.p.lexema+" "+lexicomain.p.token+" "+lexicomain.p.renglon);
+                 lexicomain.p = lexicomain.p.sig;
              }
                
          }
